af5c8456195011e1ff59c1a7d358916a6ca1939b
rin-11 <erinkates11@gmail.com>
2024-01-29 14:02:25 -0500

    header update

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from 'react';
+import MenuItem from './MenuItem';
+import { AiFillHome } from 'react-icons/ai';
+import { BsFillInfoCircleFill } from 'react-icons/bs';
 
 export default function Header() {
   return (
-    <div>Header</div>
+   <div className="">
+        <MenuItem title='home' address='/' Icon={AiFillHome} />
+        <MenuItem title='about' address='/about' Icon={BsFillInfoCircleFill} />
+   </div>
   )
 }
