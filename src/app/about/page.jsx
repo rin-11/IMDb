a652e4ac40d8665efe4d4af7bdc23cd70fd18954
rin-11 <erinkates11@gmail.com>
2024-01-30 13:11:25 -0500

    about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,35 @@
+export default function About() {
+    return (
+      <div className='max-w-6xl mx-auto p-3 space-y-4'>
+        <h1 className='text-2xl font-medium text-neutral-500'>About this Website</h1>
+        <p>
+          Welcome to my movie database website! This app was built as a one-stop destination for
+          all your movie-related needs.
+        </p>
+  
+        <p>
+          This website is designed to provide you with a comprehensive database of
+          movies from all around the world, along with the latest news, reviews,
+          and trailers. This movie database is constantly updated with new
+          releases, ensuring that you have access to the latest and greatest in
+          the world of cinema. You can search for movies by title, director,
+          actor, genre, or release date, making it easy to find the perfect movie
+          for any occasion.
+        </p>
+  
+        <p>
+          In addition to an extensive movie database, this website also offer a platform
+          for movie lovers to connect and share their thoughts on the latest
+          releases. Our community section includes a forum where you can discuss
+          your favorite films with like-minded individuals and read reviews and
+          ratings from other users. There is also a section dedicated to movie news
+          and trailers, keeping you updated with the latest happenings in the
+          world of cinema. Thank you for visiting my website and I hope you
+          enjoy your time browsing through our movie database. If you have any
+          feedback or suggestions, please feel free to contact me. I am always
+          looking for ways to improve and enhance the user experience on our
+          website. Happy browsing!
+        </p>
+      </div>
+    );
+  }
